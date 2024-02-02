ade659568392af7d5968c377b664e24c3090541f
kys0119 <sunnkys119@naver.com>
2024-02-02 23:53:30 +0900

    ..

--- app/src/main/java/com/example/coachconnect/MainActivity.kt
+++ app/src/main/java/com/example/coachconnect/MainActivity.kt
@@ -15,44 +15,6 @@ import android.util.Log
 class MainActivity : AppCompatActivity() {
     private val RESULT_CODE_BOOKING = 1 // 예약 결과 코드
 
-    private fun parseYourTime(timeString: String?): Date? {
-        return try {
-            val formatter = SimpleDateFormat("hh:mm a", Locale.getDefault())
-            formatter.parse(timeString)
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    private val someActivityResultLauncher =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                // 처리할 내용
-                val trainerName = result.data?.getStringExtra("trainerName")
-                val selectedTimeArray: Array<String?>? =
-                    result.data?.getStringArrayExtra("selectedTimes")
-                val selectedDate = result.data?.getStringExtra("selectedDate")
-
-                // 오전과 오후를 구분하여 표시할 변수
-                val timePeriod: String = when {
-                    selectedTimeArray?.getOrNull(0)
-                        ?.contains("am", ignoreCase = true) == true -> "오전"
-                    selectedTimeArray?.getOrNull(0)
-                        ?.contains("pm", ignoreCase = true) == true -> "오후"
-                    else -> "" // 오전/오후가 아닌 경우
-                }
-
-                // 예약 정보를 표시할 TextView 찾아서 텍스트 설정
-                val bkInfo1 = findViewById<TextView>(R.id.bkInfo1)
-                val formattedTime =
-                    selectedTimeArray?.joinToString(", ") { it ?: "" } ?: ""
-                val displayText =
-                    "$trainerName 트레이너\n $selectedDate\n ${if (timePeriod.isNotBlank()) "$timePeriod " else ""}$formattedTime"
-                bkInfo1.text = displayText
-                updateReservationUI(selectedDate, selectedTimeArray ?: emptyArray())
-
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         val trainerName = intent.getStringExtra("trainerName")
         val selectedTimeArray = intent.getStringArrayExtra("selectedTimes")
         val selectedDate = intent.getStringExtra("selectedDate")
+        val trainerLocation = intent.getStringExtra("trainerLocation")
 
         //오전과 오후를 구분하여 표시할 변수
         val timePeriod: String = when {
@@ -76,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         val formattedTime = selectedTimeArray?.joinToString(", ") { it ?: "" } ?: ""
 
         val displayText =
-            "$trainerName 트레이너\n $selectedDate\n ${if (timePeriod.isNotBlank()) "$timePeriod " else ""}$formattedTime"
+            "$trainerName 트레이너\n $selectedDate\n ${if (timePeriod.isNotBlank()) "$timePeriod " else ""}$formattedTime\n $trainerLocation"
         bkInfo1.text = displayText
         updateReservationUI(selectedDate, selectedTimeArray ?: emptyArray())
 
@@ -89,10 +52,11 @@ class MainActivity : AppCompatActivity() {
         search_ic.setOnClickListener {
             // 효과 (아이콘 축소)
             it.animate().scaleX(0.8f).scaleY(0.8f).setDuration(100).withEndAction {
-                someActivityResultLauncher.launch(Intent(this, TrainerList::class.java))
+                val intent = Intent(this, TrainerList::class.java)
+                startActivity(intent)
                 // 효과 제거 (아이콘 확대)
                 it.animate().scaleX(1f).scaleY(1f).setDuration(100).start()
-            }
+            }.start()
         }
         happy_ic.setOnClickListener {
             // 효과 (아이콘 축소)
